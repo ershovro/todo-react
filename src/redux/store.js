a222f4a90432e6ce84fe4885e4e1531d711d5a89
ro.ershov <ro.ershov@tensor.ru>
2021-06-07 17:47:01 +0300

    added middleware

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,4 +1,5 @@
-import {createStore} from 'redux';
+import {createStore, applyMiddleware} from 'redux';
+import generateId from './middleware/generateId';
 import reducer from './reducer';
 
-export default createStore(reducer);
+export default createStore(reducer, applyMiddleware(generateId));
