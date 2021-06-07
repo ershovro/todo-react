ef067b8e6f74bb87bcf47deac92fa130a7211855
ro.ershov <ro.ershov@tensor.ru>
2021-06-07 14:09:36 +0300

    added title and style fix

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -5,6 +5,7 @@ import styles from './app.module.css';
 const App = () => {
    return (
       <div className={styles.app}>
+         <h1 className={styles.title}> To do </h1>
          <Tasks />
       </div>
    );
--- src/components/tasks/tasks.js
+++ src/components/tasks/tasks.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import Task from './task';
 import AddTask from './add-task';
+import styles from './tasks.module.css';
 
 const Tasks = () => {
    return (
-      <div>
+      <div className={styles.tasks}>
          <AddTask />
          <Task text="to do"/>
       </div>
